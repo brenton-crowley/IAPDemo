e81036d36a991a1d9f077dd4ffb81d71c415f87d
brenton-crowley <brenton.crowley@gmail.com>
2023-02-09 17:21:54 +1100

    Initial Commit

---
+++ IAPDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  IAPDemo
+//
+//  Created by Brent Crowley on 9/2/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IAPDemo/IAPDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAPDemoApp.swift
+//  IAPDemo
+//
+//  Created by Brent Crowley on 9/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct IAPDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
